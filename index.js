5898e7592c8cd94fcb7fae6b444c6c54018eefd4
arvind <arvindj97@gmail.com>
2021-01-12 17:25:24 +0530

    setup home-controller

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+// This is where the route (folder) is setup  using middleware
 //use express router
 app.use('/',require('./routes'));  // can also give path as - require('./routes/index') , but it by default takes index   file only, as the name of this file(entry file) nd route file is same
 

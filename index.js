8c136bcc5fd8543f4e10a275f55a113053a7a946
arvind <arvindj97@gmail.com>
2021-01-12 19:18:04 +0530

    setup ejs template engine

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port=8000;
 //use express router
 app.use('/',require('./routes'));  // can also give path as - require('./routes/index') , but it by default takes index   file only, as the name of this file(entry file) nd route file is same
 
+
+// set up the view engine
+app.set('view engine','ejs');  //app.set(name,value) here using set() we set the values in the 'app' object
+app.set('views','./views');
+
 app.listen(port,function(err){
   if(err){console.log(`Error while setting up the server: ${err}`); return;}
 

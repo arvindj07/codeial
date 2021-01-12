ab78346afccd5499db248c6cf15ab23d7c37fd37
arvind <arvindj97@gmail.com>
2021-01-12 17:04:52 +0530

    set up router

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//use express router
+app.use('/',require('./routes'));  // can also give path as - require('./routes/index') , but it by default takes index   file only, as the name of this file(entry file) nd route file is same
+
 app.listen(port,function(err){
   if(err){console.log(`Error while setting up the server: ${err}`); return;}
 

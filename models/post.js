74925e0c91c97fe1efd9c6b8c41d25d9d0c84b3e
arvind <arvindj97@gmail.com>
2021-01-17 13:47:38 +0530

    Create Comment Schema and add comment array fields to Post Schema

--- models/post.js
+++ models/post.js
@@ -9,7 +9,15 @@ const postSchema = new mongoose.Schema({
   user:{
     type: mongoose.Schema.Types.ObjectId,   // to store only the id of user
     ref: 'User'                             //  refering to User Schema for this filed(i.e., user)
-  }
+  },
+
+  //include the array of ids of all comments in this Post Schema itself
+  comments:[
+    {
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'Comment'
+    }
+  ]
 
 },{
   timestamps:true

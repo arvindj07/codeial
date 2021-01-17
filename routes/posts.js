ee0e2828e26ab3b08703113ac897b9bb04a818aa
arvind <arvindj97@gmail.com>
2021-01-17 13:23:54 +0530

    Setup two-lvl checks i.e, Authentication for Creating a Post

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 
 const postController = require('../controllers/posts_controller');
 
-router.post('/create',postController.create) 
+//only authenticated User can create a Post
+router.post('/create',passport.checkAuthentication,postController.create) 
 
 module.exports = router;

ed9def7b10154a97b2398f8309b322fe8dae1cc9
arvind <arvindj97@gmail.com>
2021-01-17 13:13:35 +0530

    Display Posts and Related User by Pre-populating user in Post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,15 +1,21 @@
 // for every action in the controller u have to setup the route, controllers are collections of actions
+const Post = require('../models/post');
 
 module.exports.home = function(req,res){
   // to display the cookie passed in req using Inspect tool in browser
   // console.log(req.cookies);
-
   //to access the cookie and reset it while sending back response to browser
-  res.cookie('user_id',25);
+  // res.cookie('user_id',25);
 
-  return res.render('home',{
-    title:'Home'
+  // to get all the posts in Post DB/collection and populate the User of each post
+  Post.find({}).populate('user').exec(function(err,posts){
+    return res.render('home',{
+      title:'Codieal | Home',
+      posts: posts
+    });
   });
+
+    
 }
 
 // module.exports.actionName= function(req,res){}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,13 @@
+const Post= require('../models/post');
+
 module.exports.create =function(req,res){
-  return res.end('Create');
+  Post.create({
+    content: req.body.content,
+    user: req.user._id,       // getting this user from Passport strategy, from callback func done() whne authenticated
+  },function(err,post){
+    if(err){console.log('error in creating the post'); return;}
+
+    return res.redirect('back');
+
+  });
 }

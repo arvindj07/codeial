500f079c99726e9f6aff931fd567eecd525cc51b
arvind <arvindj97@gmail.com>
2021-01-26 23:00:20 +0530

    Delete Posts using API

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,7 +1,9 @@
 // Its like posts_controller , but For APIs
 
 const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
 
+// in this ACTION we r just getting the info of all the posts and returning it in JSON object, when api is used, in this case, the API request is made by Postman API app, we r just responding to the request send by Postman
 module.exports.index= async function(req,res){
 
 //We r passing all the posts through Json obj,so first we r collecting all the posts from Post Schema
@@ -21,4 +23,39 @@ module.exports.index= async function(req,res){
     message:"List of Posts in v1",
     posts:posts  // Passing all the posts from the Post Schema
   });
+}
+
+// in this ACTION we r deleting the Post selected(i.e., a particular post is idetified using the id params,which is passed along with the api call in Postman) and associated comments and then returning the JSON object with a message of successful Deletion 
+// NOTE: we r not checking for Authenticated Users here, as there r none, we r just checking the whether the api is working or not
+module.exports.destroy = async function(req,res){
+
+  try{
+
+    // check if post exists
+    let post = await Post.findById(req.params.id);
+
+    // checking whether the user who is deleting the Post is the one who Created it
+    // .id means converting the object id into string(so not using _id)
+    // here post.user gives id as its not populated yet
+    // if(post.user ==req.user.id ){
+      post.remove();        // post deleted
+
+       await Comment.deleteMany({post:req.params.id});
+
+       return res.json(200,{
+        message:"Post and associated comments Deleted!"
+       });
+
+    // }else{
+    //   req.flash('error','Not Authorized to Delete Post');
+    //   return res.redirect('back');
+    // }
+
+  }catch(err){
+    // req.flash('error',err);
+    return res.json(500,{
+      message:"Internal Server Error"
+    });
+  }
+  
 }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -4,5 +4,7 @@ const router=express.Router();
 const postApi = require('../../../controllers/api/v1/posts_api'); // Post_api controller
 
 router.get('/',postApi.index);// The link to access this route is- http://localhost:8000/api/v1/posts
+router.delete('/:id',postApi.destroy); // To delete the Post, we r passing the id of the post as well to delete that     
+                                    //particular post. We r using DELETE req, as we r making an api req to server using API-Postman
 
 module.exports= router;

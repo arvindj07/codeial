73a38d354852d7ab5f74493766c48f7c5565a1ed
arvind <arvindj97@gmail.com>
2021-01-29 14:14:25 +0530

    Setting up Nodemailer, Configuring it

---
+++ config/nodemailer.js
@@ -0,0 +1,44 @@
+const nodemailer = require('nodemailer');
+const ejs= require('ejs');
+const path= require('path');
+
+//Defines the configuration using which we will be sending email
+
+// This is the part which sends the email,
+//The connection with the MAILING SERVER is set here
+let transporter = nodemailer.createTransport({
+  service:'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {   // The gmail- account details of user to Establish their identity with gmail
+    user: 'tommycruise1997', // user
+    pass: 'ron@ldo7', // password
+  },
+});
+
+//Definies the TEMPLATE that is to be used
+
+//Whenever we r sending html email, there exists some html/ejs files which we will be sending to the user 
+//This func sets the path of those files
+// relativePath is the place from where ,this func is called 
+let renderTemplate= (data,relativePath) => {
+  let mailHtml;
+  ejs.renderFile(
+    path.join(__dirname,'../views/mailers',relativePath), // this path will contain all the html/ejs Templates
+    data,  // the context of ejs file
+    function(err,template){
+      if(err){console.log('error in rendering template: ',err); return ;}
+
+      mailHtml=template;
+    }
+  )
+
+  return mailHtml;
+}
+
+// Exporting 2 properties
+module.exports= {
+  transporter:transporter,
+  renderTemplate: renderTemplate,
+};

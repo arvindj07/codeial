cd6c177d2a3f78f245309f69db0b477ad2653f60
arvind <arvindj97@gmail.com>
2021-01-30 14:40:36 +0530

    Setting up The Connections using Socket

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,2 +1,27 @@
 //Its the Subscriber
 
+//This Class is used to send a request for Connection (front-end side connection)
+//This class in Initialized in Home.ejs
+class ChatEngine{
+  constructor(chatBoxId,userEmail){
+    this.chatBoxId=$(`#${chatBoxId}`);
+    this.userEmail= userEmail;
+
+    //to initiate the connection
+    // the cdn file for socket ,at home.ejs in views gives us this object -'io'
+    //this line fires an event to connect
+    this.socket=io.connect('http://localhost:5000');
+
+    if(this.userEmail){
+      this.connectionHandler();
+    }
+  }
+
+  // it handles the to-nd-fro interaction bw subcriber(user) and observer(server)
+  connectionHandler(){
+    // handling the event- 'connect', which checks if the connection has been established by server or not (connection established at config-> chat_sockets.js)
+    this.socket.on('connect',function(){
+      console.log('Connection established using Sockets..!');
+    });
+  }
+}
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,4 +1,22 @@
 // Its the Observer/Server
-modeule.exports.chatSockets = function(socketServer){
 
+//Receive the request for connection
+module.exports.chatSockets = function(socketServer){
+  let io = require('socket.io')(socketServer, {
+    cors: {                                     // NOTE: This is an extra bit that came up after searching for the error 
+                                                // in StackOverflow, as the connection wasnt established
+      origin: "http://localhost:8000",
+      credentials: true
+    }
+  });
+
+  //This event is called 'connection' over here nd 'connect' at user/front-end side,But usually the event occurring over there and here has same name
+  // here we r handling the event connect that has been triggered at user side and establish the connection
+  io.sockets.on('connection',function(socket){
+    console.log('New Connection received',socket.id);
+
+    socket.on('disconnect',function(){
+      console.log('socket Disconnected');
+    });
+  });
 }
